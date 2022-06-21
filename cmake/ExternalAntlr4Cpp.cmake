3b9af80c9a73a95a0386403ab811789fde4fb7ae
Omar Ibrahim <moar.ahmed@gmail.com>
2022-06-21 10:47:50 +0200

    uncomment cmake lines

--- cmake/ExternalAntlr4Cpp.cmake
+++ cmake/ExternalAntlr4Cpp.cmake
@@ -94,8 +94,8 @@ else()
     ExternalProject_Add(
             antlr4_runtime
             PREFIX antlr4_runtime
-#            GIT_REPOSITORY ${ANTLR4_GIT_REPOSITORY}
-#            GIT_TAG ${ANTLR4_TAG}
+            GIT_REPOSITORY ${ANTLR4_GIT_REPOSITORY}
+            GIT_TAG ${ANTLR4_TAG}
             DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
             BUILD_COMMAND ""
             BUILD_IN_SOURCE 1
